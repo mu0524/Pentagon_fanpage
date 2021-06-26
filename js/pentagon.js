d5c7750437eff5cf0570b86be7f6629dc558a132
mu0524 <aays2025@gmail.com>
2021-06-26 19:04:58 +0800

    update info

--- js/pentagon.js
+++ js/pentagon.js
@@ -18,6 +18,26 @@ function img_romantic_out() {
     $("#img_romantic").css("opacity","1");
 }
 
+function img_Sporty_in() {
+    $("#img_Sporty_ro").css("opacity","1");
+    $("#img_Sporty").css("opacity","0");
+}
+
+function img_Sporty_out() {
+    $("#img_Sporty_ro").css("opacity","0");
+    $("#img_Sporty").css("opacity","1");
+}
+
+function img_Mild_in() {
+    $("#img_Mild_ro").css("opacity","1");
+    $("#img_Mild").css("opacity","0");
+}
+
+function img_Mild_out() {
+    $("#img_Mild_ro").css("opacity","0");
+    $("#img_Mild").css("opacity","1");
+}
+
 // function img_romantic_in() {
 //     $("#img_romantic").attr("src","./img/pentagon-ro.png");
 // }
